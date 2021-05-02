ef3ebeb0ae1eb3e5c9a4a8f8d99c5ec2316ebda2
Davi Lucas <davilucasdlcreator@gmail.com>
2021-05-02 19:20:47 -0300

    :bug: fixing path servet

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(path.join(__dirname, 'server.json'))
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000;
  
